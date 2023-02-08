8c56f43eae15f9c5192de0f4fab1fc722c4348e3
annapmarin <annapratsmarin@gmail.com>
2023-02-08 19:18:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+const element1 = document.getElementById("element1");
+
+element1.addEventListener("click", changeClass1);
+
+function changeClass1() {
+  element1.classList.toggle("container__element--active");
+  element1.classList.toggle("container__element");
+
+  if (element2.classList.contains("container__element--active")) {
+    element2.classList.remove("container__element--active");
+    element2.classList.add("container__element");
+  }
+
+  if(element3.classList.contains("container__element--active")) {
+    element3.classList.remove("container__element--active");
+    element3.classList.add("container__element");
+  }
+
+  if(element4.classList.contains("container__element--active")) {
+    element4.classList.remove("container__element--active");
+    element4.classList.add("container__element");
+  }
+
+  if(element5.classList.contains("container__element--active")) {
+    element5.classList.remove("container__element--active");
+    element5.classList.add("container__element");
+  }
+};
+
+
+
+const element2 = document.getElementById("element2");
+
+element2.addEventListener("click", changeClass2);
+
+function changeClass2() {
+  element2.classList.toggle("container__element--active");
+  element2.classList.toggle("container__element");
+
+  if (element1.classList.contains("container__element--active")) {
+    element1.classList.remove("container__element--active");
+    element1.classList.add("container__element");
+  }
+
+  if(element3.classList.contains("container__element--active")) {
+    element3.classList.remove("container__element--active");
+    element3.classList.add("container__element");
+  }
+
+  if(element4.classList.contains("container__element--active")) {
+    element4.classList.remove("container__element--active");
+    element4.classList.add("container__element");
+  }
+
+  if(element5.classList.contains("container__element--active")) {
+    element5.classList.remove("container__element--active");
+    element5.classList.add("container__element");
+  }
+};
+
+
+
+const element3 = document.getElementById("element3");
+
+element3.addEventListener("click", changeClass3);
+
+function changeClass3() {
+  element3.classList.toggle("container__element--active");
+  element3.classList.toggle("container__element");
+
+  if (element1.classList.contains("container__element--active")) {
+    element1.classList.remove("container__element--active");
+    element1.classList.add("container__element");
+  }
+
+  if(element2.classList.contains("container__element--active")) {
+    element2.classList.remove("container__element--active");
+    element2.classList.add("container__element");
+  }
+
+  if(element4.classList.contains("container__element--active")) {
+    element4.classList.remove("container__element--active");
+    element4.classList.add("container__element");
+  }
+
+  if(element5.classList.contains("container__element--active")) {
+    element5.classList.remove("container__element--active");
+    element5.classList.add("container__element");
+  }
+};
+
+
+
+const element4 = document.getElementById("element4");
+
+element4.addEventListener("click", changeClass4);
+
+function changeClass4() {
+  element4.classList.toggle("container__element--active");
+  element4.classList.toggle("container__element");
+
+  if (element1.classList.contains("container__element--active")) {
+    element1.classList.remove("container__element--active");
+    element1.classList.add("container__element");
+  }
+
+  if(element2.classList.contains("container__element--active")) {
+    element2.classList.remove("container__element--active");
+    element2.classList.add("container__element");
+  }
+
+  if(element3.classList.contains("container__element--active")) {
+    element3.classList.remove("container__element--active");
+    element3.classList.add("container__element");
+  }
+
+  if(element5.classList.contains("container__element--active")) {
+    element5.classList.remove("container__element--active");
+    element5.classList.add("container__element");
+  }
+};
+
+
+
+const element5 = document.getElementById("element5");
+
+element5.addEventListener("click", changeClass5);
+
+function changeClass5() {
+  element5.classList.toggle("container__element--active");
+  element5.classList.toggle("container__element");
+
+  if (element1.classList.contains("container__element--active")) {
+    element1.classList.remove("container__element--active");
+    element1.classList.add("container__element");
+  }
+
+  if(element2.classList.contains("container__element--active")) {
+    element2.classList.remove("container__element--active");
+    element2.classList.add("container__element");
+  }
+
+  if(element3.classList.contains("container__element--active")) {
+    element3.classList.remove("container__element--active");
+    element3.classList.add("container__element");
+  }
+
+  if(element4.classList.contains("container__element--active")) {
+    element4.classList.remove("container__element--active");
+    element4.classList.add("container__element");
+  }
+};
+
+
+
